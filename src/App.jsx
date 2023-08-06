625b29b83815ba6427fd179fada6d0124c0e9ddd
KausaChangs <changayakausa@gmail.com>
2023-08-06 16:32:54 +0200

    started the board premise

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0);
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const App = () => {
+  const createBoard = () => {
+    //we are looping through 8*8=64 times because thats the amount of squares we need to make our board
+    for (let i = 0; i < width * width; i++) {
+      const randomColors =
+        candyColors[Math.floor(Math.random * candyColors.length)];
+    }
+  };
 
   return <div></div>;
-}
+};
 
 export default App;
