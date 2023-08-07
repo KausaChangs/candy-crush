1ba066fe872681789a396661787f64d2d726ccb9
KausaChangs <changayakausa@gmail.com>
2023-08-07 14:21:58 +0200

    created an array that logs out 64 random colors/need to fix why its logging twice

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,19 @@ const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
 const App = () => {
   const createBoard = () => {
     //we are looping through 8*8=64 times because thats the amount of squares we need to make our board
-
     const randomColorArrangement = [];
     for (let i = 0; i < width * width; i++) {
       const randomNumbersFrom0to5 = [
-        Math.floor(Math.random * candyColors.length),
+        Math.floor(Math.random() * candyColors.length),
       ];
       const randomColor = candyColors[randomNumbersFrom0to5];
 
       randomColorArrangement.push(randomColor);
     }
+    console.log(randomColorArrangement);
   };
+
+  createBoard();
 
   return <div></div>;
 };
